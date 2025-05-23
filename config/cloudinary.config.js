dfc0ffd5273de38549b2f9709466772e361960c9
Edgar Garcia <edgargarciafcb@gmail.com>
2025-05-22 18:59:51 -0600

    fixed cloudinary configuration

--- config/cloudinary.config.js
+++ config/cloudinary.config.js
@@ -1,24 +1,19 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-
-// Configura Cloudinary con tus credenciales (puedes poner estas credenciales en variables de entorno .env)
 cloudinary.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// Configuración de almacenamiento con Multer para subir directamente a Cloudinary
 const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+  cloudinary,
   params: {
-    folder: 'productos', // Carpeta en Cloudinary donde se almacenarán las imágenes
-    allowedFormats: ['jpg', 'jpeg', 'png'], // Formatos de imagen permitidos
+    folder: "productos",
+    allowed_formats: ["jpg", "jpeg", "png"],
+    public_id: (req, file) => Date.now() + "-" + file.originalname,
   },
 });
 
-// Inicializa Multer con el almacenamiento en Cloudinary
-const upload = require('multer')({ storage });
-
-module.exports = { cloudinary, upload };
+module.exports = { cloudinary, storage };
